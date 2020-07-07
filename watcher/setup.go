3dd2f17f6586fc0e01f1d15498a442e5c3b569ef
Josep Jesus <josep@prowarehouse.nl>
2020-07-07 16:09:12 +0200

    Add route

---
+++ watcher/setup.go
@@ -0,0 +1,21 @@
+package watcher
+
+import "github.com/gin-gonic/gin"
+
+func Setup(engine *gin.Engine) {
+	// Load HTML templates
+	engine.LoadHTMLGlob("static/templates/*")
+	// Use the logging middleware
+	engine.Use(RequestLogger)
+	engine.Use(ResponseLogger)
+	// Re route to static folders
+	engine.Static("/js", "./static/js/")
+	engine.Static("/css", "./static/css")
+	// Setup necessary routes
+	watcherGroup := engine.Group("/watcher")
+	watcherGroup.GET("/", Dashboard)
+	watcherGroup.GET("/requests", GetRequests)
+	watcherGroup.GET("/requests/:id", ShowRequest)
+	watcherGroup.GET("/responses", GetResponses)
+	watcherGroup.GET("/responses/:id", ShowResponse)
+}
