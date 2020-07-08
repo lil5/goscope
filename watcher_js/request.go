92ece05d9737a5e7091d6d24d8ab2590909b77d1
Josep Jesus <josep@prowarehouse.nl>
2020-07-08 09:54:23 +0200

    add tab ui

--- watcher_js/request.go
+++ watcher_js/request.go
@@ -3,4 +3,13 @@ const RequestJs = `
 document.addEventListener("DOMContentLoaded", function () {
     hljs.initHighlightingOnLoad();
 });
+
+function openTab(tabName) {
+  var i;
+  var x = document.getElementsByClassName("view-tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
 `
--- watcher_templates/request.go
+++ watcher_templates/request.go
@@ -19,8 +19,12 @@ var RequestTemplate = fmt.Sprintf(`
 <body>
 
 <div class="m-3 p-3 text-center" style="line-height: 2em;">
-    <h1 class="font-xl m-2">Request at {{.REQUEST_TIME}} - {{.APPLICATION_NAME}} - Go Watcher</h1>
-    <div class="md:w-2/3 lg:w-2/3 text-left" style="margin:0 auto;">
+	 <div class="w3-bar w3-black">
+	  <button class="" onclick="openTab('request-tab')">Request</button>
+	  <button class="" onclick="openTab('response-tab')">Response</button>
+	</div> 
+    <div id="request-tab" class="view-tab md:w-2/3 lg:w-2/3 text-left" style="margin:0 auto;">
+		<h1 class="font-xl m-2">Request at {{.REQUEST_TIME}} - {{.APPLICATION_NAME}} - Go Watcher</h1>
         <p>Client IP Address:
         <pre><code class="plaintext">{{.REQUEST_CLIENT_IP}}</code></pre>
         </p>
@@ -54,34 +58,33 @@ var RequestTemplate = fmt.Sprintf(`
         <p>Body:
         <pre><code class="language-json">{{.REQUEST_BODY}}</code></pre>
         </p>
-
-        <div class="md:w-2/3 lg:w-2/3 text-left" style="margin:0 auto;">
-            <h1 class="font-xl m-2">Response at {{.RESPONSE_TIME}}</h1>
-            <p>Client IP Address:
-            <pre><code class="plaintext">{{.RESPONSE_CLIENT_IP}}</code></pre>
-            </p>
-            <p>Status:
-            <pre><code class="plaintext">{{.RESPONSE_STATUS}}</code></pre>
-            </p>
-            <p>Path:
-            <pre><code class="plaintext">{{.RESPONSE_PATH}}</code></pre>
-            </p>
-            <p>Time:
-            <pre><code class="plaintext">{{.RESPONSE_TIME}}</code></pre>
-            </p>
-            <p>Uid:
-            <pre><code class="plaintext">{{.RESPONSE_UID}}</code></pre>
-            </p>
-            <p>Size:
-            <pre><code class="plaintext">{{.RESPONSE_SIZE}}</code></pre>
-            </p>
-            <p>Headers:
-            <pre><code class="language-json">{{.RESPONSE_HEADERS}}</code></pre>
-            <p>Body:
-            <pre><code class="language-json">{{.RESPONSE_BODY}}</code></pre>
-            </p>
-        </div>
     </div>
+	<div id="response-tab" class="view-tab md:w-2/3 lg:w-2/3 text-left" style="margin:0 auto;">
+		<h1 class="font-xl m-2">Response at {{.RESPONSE_TIME}}</h1>
+		<p>Client IP Address:
+		<pre><code class="plaintext">{{.RESPONSE_CLIENT_IP}}</code></pre>
+		</p>
+		<p>Status:
+		<pre><code class="plaintext">{{.RESPONSE_STATUS}}</code></pre>
+		</p>
+		<p>Path:
+		<pre><code class="plaintext">{{.RESPONSE_PATH}}</code></pre>
+		</p>
+		<p>Time:
+		<pre><code class="plaintext">{{.RESPONSE_TIME}}</code></pre>
+		</p>
+		<p>Uid:
+		<pre><code class="plaintext">{{.RESPONSE_UID}}</code></pre>
+		</p>
+		<p>Size:
+		<pre><code class="plaintext">{{.RESPONSE_SIZE}}</code></pre>
+		</p>
+		<p>Headers:
+		<pre><code class="language-json">{{.RESPONSE_HEADERS}}</code></pre>
+		<p>Body:
+		<pre><code class="language-json">{{.RESPONSE_BODY}}</code></pre>
+		</p>
+	</div>
 </div>
 <script>%s</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js"></script>
