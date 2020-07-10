a5b25371df61f1d6d8ecb5be10ad6a0e247e4575
Josep Jesus <josep@prowarehouse.nl>
2020-07-10 11:04:04 +0200

    fix detailed log page

--- goscope_templates/log.go
+++ goscope_templates/log.go
@@ -18,13 +18,15 @@ func LogView() string {
 </head>
 <body>
 <div class="m-3 p-3 text-center" style="line-height: 2em;">
-	<h1 class="font-xl m-2">Request at {{.REQUEST_TIME}} - {{.APPLICATION_NAME}} - GoScope</h1>
-	<p>Time:
-	<pre><code class="plaintext">{{.TIME}}</code></pre>
-	</p>
-	<p>Message:
-	<pre><code class="plaintext">{{.MESSAGE}}</code></pre>
-	</p>
+	<div class="md:w-2/3 lg:w-2/3 text-left" style="margin:0 auto;">
+		<h1 class="font-xl m-2">Logged at {{.TIME}} - {{.APPLICATION_NAME}} - GoScope</h1>
+		<p>Time:
+		<pre><code class="plaintext">{{.TIME}}</code></pre>
+		</p>
+		<p>Message:
+		<pre><code class="plaintext">{{.MESSAGE}}</code></pre>
+		</p>
+	</div>
 </div>
 <script>%s</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js"></script>
