bbbeaf5d09c8b8721e4a62e410ffbd72ffa9f0ea
Josep Jesus <josep@prowarehouse.nl>
2020-07-19 14:05:49 +0200

    #4 reduce template repetition

--- goscope_templates/log.go
+++ goscope_templates/log.go
@@ -10,19 +10,9 @@ func LogView() string {
 	const template = `<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Request at {{.TIME}} - {{.APPLICATION_NAME}}</title>
-    <link href="https://cdn.jsdelivr.net/gh/tretapey/raisincss@1.1.0/raisin.min.css" rel="stylesheet"/>
-	<link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet"> 
-	<link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet"> 
+    %s
     <style>%s</style>
     <style>%s</style>
-	<link rel="apple-touch-icon" sizes="180x180" href="https://pro-warehouse-res.cloudinary.com/image/upload/v1595014987/git-repositories/goscope/application/lrhheuzhlvgtsxyayru3.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="https://pro-warehouse-res.cloudinary.com/image/upload/v1595015010/git-repositories/goscope/application/blaaguzlyw9s5x4jbhgq.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="https://pro-warehouse-res.cloudinary.com/image/upload/v1595015010/git-repositories/goscope/application/bchsism3hubielueb6xk.png">
-	<link rel="mask-icon" href="https://pro-warehouse-res.cloudinary.com/image/upload/v1595015042/git-repositories/goscope/application/wswxoxpwirad8udbkwz8.svg" color="#5bbad5">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="theme-color" content="#ffffff">
 </head>
 <body>
 <div class="m-3 p-3 text-center" style="line-height: 2em;">
@@ -40,5 +30,5 @@ func LogView() string {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js"></script>
 </body>
 </html>`
-	return fmt.Sprintf(template, goscope_css.HighlightTheme(), goscope_css.WatcherStyles(), goscope_js.LogJs())
+	return fmt.Sprintf(template, CommonHead,goscope_css.HighlightTheme(), goscope_css.WatcherStyles(), goscope_js.LogJs())
 }
--- goscope_templates/logs.go
+++ goscope_templates/logs.go
@@ -11,19 +11,9 @@ func LogsView() string {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>{{.APPLICATION_NAME}} Logs - GoScope</title>
-    <link href="https://cdn.jsdelivr.net/gh/tretapey/raisincss@1.1.0/raisin.min.css" rel="stylesheet"/>
-	<link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet"> 
-	<link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet"> 
+    %s
     <style>%s</style>
     <style>%s</style>
-	<link rel="apple-touch-icon" sizes="180x180" href="https://pro-warehouse-res.cloudinary.com/image/upload/v1595014987/git-repositories/goscope/application/lrhheuzhlvgtsxyayru3.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="https://pro-warehouse-res.cloudinary.com/image/upload/v1595015010/git-repositories/goscope/application/blaaguzlyw9s5x4jbhgq.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="https://pro-warehouse-res.cloudinary.com/image/upload/v1595015010/git-repositories/goscope/application/bchsism3hubielueb6xk.png">
-	<link rel="mask-icon" href="https://pro-warehouse-res.cloudinary.com/image/upload/v1595015042/git-repositories/goscope/application/wswxoxpwirad8udbkwz8.svg" color="#5bbad5">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="theme-color" content="#ffffff">
 </head>
 <body>
 %s
@@ -43,5 +33,5 @@ func LogsView() string {
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </body>
 </html>`
-	return fmt.Sprintf(template, goscope_css.HighlightTheme(), goscope_css.WatcherStyles(), NavbarComponent("LOGS"), FooterText, goscope_js.JsUtils(), goscope_js.LogsJs())
+	return fmt.Sprintf(template, CommonHead, goscope_css.HighlightTheme(), goscope_css.WatcherStyles(), NavbarComponent("LOGS"), FooterText, goscope_js.JsUtils(), goscope_js.LogsJs())
 }
