7615954e2c0a2183ea8e6e603253777a6c40c077
Lucian Last <lucian@prowarehouse.nl>
2020-10-08 08:28:43 +0200

    Add filter and angle icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,12 +8,25 @@ import {
   faClipboardList,
   faEye,
   faSearch,
-  faTimes
+  faTimes,
+  faFilter,
+  faAngleLeft,
+  faAngleRight
 } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faSync, faServer, faClipboardList, faEye, faSearch, faTimes);
+library.add(
+  faSync,
+  faServer,
+  faClipboardList,
+  faEye,
+  faSearch,
+  faTimes,
+  faAngleLeft,
+  faAngleRight,
+  faFilter
+);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
